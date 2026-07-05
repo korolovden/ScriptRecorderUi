8418ef2bc8ce84bfc1f82d5188c86ca86e995f05
korolovden <48968633+korolovden@users.noreply.github.com>
2026-07-05 15:48:14 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,11 @@
+namespace ScriptRecorderUI;
+
+internal static class Program
+{
+    [STAThread]
+    static void Main()
+    {
+        ApplicationConfiguration.Initialize();
+        Application.Run(new Form1());
+    }
+}
